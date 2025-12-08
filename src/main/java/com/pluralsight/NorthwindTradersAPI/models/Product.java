9bad49556aad8238001fe3a322d7d685754e42e8
Muhammad Mustafa <acmuhammadmustafa@gmail.com>
2025-12-08 15:11:20 -0500

    working on ProductController and added products, category, and categoryController classes added

---
+++ src/main/java/com/pluralsight/NorthwindTradersAPI/models/Product.java
@@ -0,0 +1,60 @@
+package com.pluralsight.NorthwindTradersAPI.models;
+
+public class Product {
+    private int productID;
+    private String productName;
+    private int categoryID;
+    private double unitPrice;
+
+    public Product() {
+    }
+
+    public Product(int productID, String productName, int categoryID, double unitPrice) {
+        this.productID = productID;
+        this.productName = productName;
+        this.categoryID = categoryID;
+        this.unitPrice = unitPrice;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Product[" +
+                " productID  = " + productID +
+                " | productName = " + productName +
+                " | categoryID = " + categoryID +
+                " | unitPrice = " + unitPrice +
+                ']';
+    }
+}
